5d369b3e55519779b93b1b530c96d100d2140382
Rik Schoonbeek <rik.schoonbeek@gmail.com>
2022-12-04 14:17:23 +0100

    First commit

---
+++ hardhat-dir/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x23897418A0b30bCCF2055e33104D139DDe585b36";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
